16efef94c4e78545f607a7244ea41addb742dcb2
Sneha Mishra <sneham398@gmail.com>
2022-05-10 12:39:48 +0530

    commit

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -27,7 +27,7 @@ const MenuCard = ({ menuData }) => {
                                         <div className="card-read">Read</div>
                                     </div>
 
-                                    <img src={image} alt="images" className='caed-media' />
+                                    <img src={image} alt="images" className='card-media' />
 
                                     <span className="card-tag subtle">Order Now</span>
                                 </div>
